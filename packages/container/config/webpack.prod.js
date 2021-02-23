3edfe18a527c2c0afe9d9d7ffe9d266e845350b1
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2021-02-23 22:11:29 +0530

    some code updation as per project

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,15 +8,13 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
-        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
