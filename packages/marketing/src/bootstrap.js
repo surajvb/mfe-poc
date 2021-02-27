73fcbde409c96f9e891fb69605f5e6bb002b4431
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2021-02-28 02:55:08 +0530

    Integrating auth in container

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -6,10 +6,13 @@ import App from './App';
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory }) => {
   const history = defaultHistory || createMemoryHistory();
+
   if (onNavigate) {
     history.listen(onNavigate);
   }
+
   ReactDOM.render(<App history={history} />, el);
+
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
